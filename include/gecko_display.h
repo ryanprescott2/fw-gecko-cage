bb770d4d2339d0f50747bded06185b4aea84a5fd
Ryan Prescott <johnryanprescott@gmail.com>
2025-09-01 21:51:04 -0700

    working temp and display

--- include/gecko_display.h
+++ include/gecko_display.h
@@ -28,18 +28,50 @@ class GeckoDisplay {
 
     // Clear the buffer
     display_.clearDisplay();
-
-    // Draw a single pixel in white
-    display_.drawPixel(10, 10, SSD1306_WHITE);
   }
 
-  void WriteTemperature(uint8_t temp) {
-    display_.clearDisplay();
+  void PrintDigits(int number) {
+    display_.print(number / 1000);
+    number = number % 1000;
+    display_.print(number / 100);
+    number = number % 100;
+    display_.print(".");
+    display_.print(number / 10);
+    number = number % 10;
+    display_.print(number);
+  }
 
-    display_.setTextSize(1);  // Normal 1:1 pixel scale
-    display_.setTextColor(SSD1306_BLACK, SSD1306_WHITE);  // Draw 'inverse' text
-    display_.setCursor(0, 0);  // Start at top-left corner
-    display_.println(F("Hello, world!"));
+  void WriteEnvironment(int temp, int humidity) {
+    display_.clearDisplay();
+    WriteTemperature(temp);
+    WriteHumidity(humidity);
+    display_.display();
+  }
+
+  void WriteTemperature(int temp) {
+    display_.setTextSize(2);               // Normal 1:1 pixel scale
+    display_.setTextColor(SSD1306_WHITE);  // Draw 'inverse' text
+    display_.setCursor(0, 0);              // Start at top-left corner
+    PrintDigits(temp);
+    display_.print(" F");
+  }
+
+  void WriteHumidity(int humidity) {
+    display_.setTextSize(2);
+    display_.setTextColor(SSD1306_WHITE);
+    display_.setCursor(0, 30);
+    PrintDigits(humidity);
+    display_.println(" %");
+  }
+
+  void DisplayError(const char* error_string) {
+    display_.clearDisplay();
+    display_.setTextSize(2);               // Normal 1:1 pixel scale
+    display_.setTextColor(SSD1306_WHITE);  // Draw 'inverse' text
+    display_.setCursor(0, 0);
+    display_.print("Error: ");
+    display_.println(error_string);
+    display_.display();
   }
 
   void WriteTime(uint8_t time) {}
--- src/main.cpp
+++ src/main.cpp
@@ -21,42 +21,107 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Arduino.h>
+#include <DHT.h>
+#include <DHT_U.h>
 #include <SPI.h>
 #include <Wire.h>
 
 #include "gecko_display.h"
 
+#define DHTPIN 2       // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT11  // DHT 11
+
+/*!
+ *  @brief  Converts Celcius to Fahrenheit
+ *  @param  c
+ *					value in Celcius
+ *	@return float value in Fahrenheit
+ */
+static float ConvertTempCtoF(float c) { return c * 1.8 + 32; }
+
 class GeckoCage {
  public:
+  static constexpr uint8_t kScreenHeight = 64;
+  static constexpr uint8_t kScreenWidth = 128;
+  static constexpr uint8_t kDhtSensorPin = 2;
+  static constexpr uint8_t kDhtType = DHT11;
+
   enum class CageState {
     kDaytime = 0,
     kNighttime = 1,
   };
 
-  static constexpr uint8_t kScreenHeight = 64;
-  static constexpr uint8_t kScreenWidth = 128;
+  void Init() {
+    gecko_display_.Init();
+    dht_.begin();
+    dht_.temperature().getSensor(&dht_sensor_);
+    dht_.humidity().getSensor(&dht_sensor_);
+    delay_ms_ = dht_sensor_.min_delay / 1000;
+  }
 
-  void Init() { gecko_display_.Init(); }
-
-  void HandleDisplayUpdate(uint8_t temperature, uint8_t time, CageState state) {
-    gecko_display_.WriteTemperature(temperature);
+  void HandleDisplayUpdate(int temp, int humidity, uint8_t time,
+                           CageState state) {
+    gecko_display_.WriteEnvironment(temp, humidity);
     gecko_display_.WriteTime(time);
   }
+
+  void ExecuteCageCycle() {
+    // read temperature and humidity
+    float temp = 0.0;
+    float humidity = 0.0;
+    if (!MeasureEnvironment(&temp, &humidity)) {
+      gecko_display_.DisplayError("DHT11 err");
+    }
+    int temp_i = temp * 100;
+    int humidity_i = humidity * 100;
+
+    // read time
+
+    // set relay state based on time
+
+    // update display
+    HandleDisplayUpdate(temp_i, humidity_i, 0, CageState::kDaytime);
+  }
+
+  bool MeasureEnvironment(float* temperature, float* humidity) {
+    sensors_event_t event;
+    dht_.temperature().getEvent(&event);
+    if (isnan(event.temperature)) {
+      return false;
+    } else {
+      *temperature = ConvertTempCtoF(event.temperature);
+    }
+    // Get humidity event and print its value.
+    dht_.humidity().getEvent(&event);
+    if (isnan(event.relative_humidity)) {
+      return false;
+    } else {
+      *humidity = event.relative_humidity;
+    }
+    return true;
+  }
+
+  uint32_t delay_ms_ = 0;
 
  private:
   GeckoDisplay gecko_display_{kScreenWidth, kScreenHeight};
   CageState state_ = CageState::kDaytime;
+  DHT_Unified dht_{kDhtSensorPin, kDhtType};
+  sensor_t dht_sensor_;
 };
 
 GeckoCage gecko_cage;
-void setup() { gecko_cage.Init(); }
+void setup() {
+  gecko_cage.Init();
+  Serial.begin(9600);
+}
 
 void loop() {
-  delay(1000);
-
-  gecko_cage.HandleDisplayUpdate(100, 4, GeckoCage::CageState::kDaytime);
+  gecko_cage.ExecuteCageCycle();
+  // gecko_cage.HandleDisplayUpdate(100, 4, GeckoCage::CageState::kDaytime);
   // read temperature
   // read time
   // set relay
   // update display
+  delay(5000);
 }
